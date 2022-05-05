853bfc5c748723f12b7ef0329cba38521bb8c190
Marco Túlio <marcotmotta@gmail.com>
2022-05-05 16:23:15 -0300

    sass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import './App.scss';
+
+import Spell from './components/spell/spell';
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <div>Sidebar</div>
+      <Spell spell_name="fireball"></Spell>
     </div>
   );
 }
---
+++ src/components/spell/spell.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Spell(props) {
+
+    return (
+        <div>{props.spell_name}</div>
+    )
+}
