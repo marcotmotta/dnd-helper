053b5adda9e366f6462f4c3e1072e55d0261fca0
Marco Túlio <marcotmotta@gmail.com>
2022-05-06 11:15:38 -0300

    dnd 5e api integration

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Spell from './components/spell/spell';
 function App() {
   return (
     <div className="App">
-      <div>Sidebar</div>
-      <Spell spell_name="fireball"></Spell>
+      {/* <div>Sidebar</div> */}
+      <Spell spell_name="lightning-bolt"></Spell>
     </div>
   );
 }
--- src/components/spell/spell.jsx
+++ src/components/spell/spell.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
 
-export default function Spell(props) {
+import { useState, useEffect } from 'react'
+
+//styles
+import "./spell.scss"
+
+import { CircularProgress } from '@mui/material';
+
+export default function Spell({ spell_name }) {
+
+    const [spell, setSpell] = useState({})
+
+    const getSpell = () => {
+        fetch(`https://www.dnd5eapi.co/api/spells/${spell_name}`)
+            .then(response => response.json())
+            .then(response => setSpell(response))
+    }
+
+    useEffect(() => {
+      getSpell()
+    }, [])
+
+    if(!Object.entries(spell).length) {
+        return (
+            <div className="spell loading"><CircularProgress /></div>
+        )
+    }
 
     return (
-        <div>{props.spell_name}</div>
+        <div className="spell">{spell.name}</div>
     )
 }
