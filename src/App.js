9551cd6235f4b4376dada718885fc5523c8cf1a4
Marco Túlio <marcotmotta@gmail.com>
2022-06-28 15:39:36 -0300

    spell search

--- src/App.js
+++ src/App.js
@@ -9,20 +9,22 @@ import {
 import Sidebar from './components/sidebar/Sidebar';
 import Spell from './components/spell/Spell';
 import School from './components/school/School';
+import SpellSearch from './components/spellSearch/SpellSearch';
 
 function App() {
   return (
     <div className="App">
-      <Sidebar />
-      <div class="app-content">
         <Router>
-          <Routes>
-            <Route path="/" element={<div>Home</div>}/>
-            <Route path="/school/:school_name" element={<School />}/>
-            <Route path="/spell/:spell_name" element={<Spell />}/>
-          </Routes>
+          <Sidebar />
+          <div class="main-content">
+            <Routes>
+              <Route path="/" element={<div>Home</div>}/>
+              <Route path="/school/:school_name" element={<School />}/>
+              <Route path="/spell/search" element={<SpellSearch />}/>
+              <Route path="/spell/:spell_name" element={<Spell />}/>
+            </Routes>
+          </div>
         </Router>
-      </div>
     </div>
   );
 }
