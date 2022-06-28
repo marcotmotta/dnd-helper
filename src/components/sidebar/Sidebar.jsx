662d405a9eb71b109cf0cd40432fb95c431f0c6a
Marco Túlio <marcotmotta@gmail.com>
2022-06-28 16:45:45 -0300

    global sass variables

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -9,8 +9,16 @@ export default function Sidebar() {
   return (
     <div className='sidebar'>
       <h1>Sidebar</h1>
-      <Link to="/spell/search">Search</Link>
-      <p>Spells</p>
+      <Link to="/spell/search"><h3>Search</h3></Link>
+      <h3>Spell Schools</h3>
+      <Link to="/school/abjuration"><p>Abjuration</p></Link>
+      <Link to="/school/conjuration"><p>Conjuration</p></Link>
+      <Link to="/school/divination"><p>Divination</p></Link>
+      <Link to="/school/enchantment"><p>Enchantment</p></Link>
+      <Link to="/school/evocation"><p>Evocation</p></Link>
+      <Link to="/school/illusion"><p>Illusion</p></Link>
+      <Link to="/school/necromancy"><p>Necromancy</p></Link>
+      <Link to="/school/transmutation"><p>Transmutation</p></Link>
     </div>
   )
 }
